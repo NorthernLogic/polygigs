02afc7cdf61602d404632d5c4881900634542d22
Adam Duren <adam@wearenorthern.com>
2016-12-29 20:34:56 -0500

    Logic to register for notifications when page loads

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -5,7 +5,11 @@ module.exports = {
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
     'bower_components/raven-js/dist/raven.min.js',
     'bower_components/raven-js/dist/raven.min.js.map',
+    'src/firebase-messaging-sw.js',
     '/**/*.png',
+  ],
+  importScripts: [
+    'src/firebase-messaging-sw.js',
   ],
   navigateFallback: '/index.html',
   navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/]
