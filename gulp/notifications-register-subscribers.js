5eb8b66f4daa7da070d9fa4cd3a9e56dc4584d7f
Adam Duren <adam@wearenorthern.com>
2016-12-30 11:37:43 -0500

    Finish up the code to register clients with a notification topic

--- gulp/notifications-register-subscribers.js
+++ gulp/notifications-register-subscribers.js
@@ -1,25 +1,70 @@
 const admin = require('firebase-admin');
+const request = require('request');
+const _ = require('lodash');
 
-const serviceAccount = require('../polygigs-firebase-adminsdk-h1d5y-22f98d5057.json');
+const serviceAccount = require(process.env.FIREBASE_ADMIN_CERT_PATH);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://polygigs.firebaseio.com'
 });
 
-module.exports = (done) => {
-  const db = admin.database();
-  const ref = db.ref('/notificationRegistrations')
-    .orderByChild('registered')
-    .equalTo(false)
+const db = admin.database();
+const registrationPath = '/notificationRegistrations';
+const topicName = gig-notification;
 
-  new Promise(resolve => {
-    ref.once('value', snapshot => resolve(snapshot.val()));
+function registerTokens(tokens) {
+  var options = {
+    uri: 'https://iid.googleapis.com/iid/v1:batchAdd',
+    method: 'POST',
+    headers: {
+      Authorization : `key=${process.env.FIREBASE_SERVER_KEY}`,
+    },
+    json: {
+      to: `/topics/${topicName}`,
+      registration_tokens: tokens,
+    }
+  };
+
+  return new Promise((resolve, reject) => {
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        resolve(_.zip(tokens, body.results));
+      } else {
+        reject(body);
+      }
+    });
   })
-  .then(unregisteredClients => {
-    console.log(unregisteredClients);
+}
+
+function updateRegistrationEntries([registrations, results]) {
+  const updatedRegistrations = _.fromPairs(results.map(([token, result]) => {
+    const updatedRegistration = Object.assign({}, registrations[token], { registered: !result.error })
+    return [token, updatedRegistration];
+  }))
+
+  return new Promise(resolve => db.ref(registrationPath).update(updatedRegistrations, () => resolve()));
+}
+
+module.exports = done => {
+  const query = db.ref(registrationPath)
+    .orderByChild('registered')
+    .equalTo(false);
+
+  const cleanup = (...args) => {
     // needed or else gulp will not exit
     db.goOffline();
     done();
+  };
+
+  new Promise(resolve => query.once('value', snapshot => resolve(snapshot.val())))
+  .then(registrations => {
+    if (!registrations) {
+      return;
+    }
+
+    return Promise.all([registrations, registerTokens(Object.keys(registrations))])
+       .then(updateRegistrationEntries);
   })
+  .then(cleanup, cleanup);
 }
